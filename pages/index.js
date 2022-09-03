b83a0c1859c635311c264d7c88b5c4fca6150d12
e.baxter@dark-coin.io <e.baxter@dark-coin.io>
2022-09-02 23:55:14 -0500

    Importing getSortedPostsData to call in getStaticProps to fetch external data from filesystem and pre-render the index page with this data.

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,47 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm Claude. I'm a property manager learning web development. You can contact me at e.baxter@dark-coin.io.</p>
+        <p>Hi, I'm Claude. This is a test page / blog to demonstrate building with next.js. You can contact me at e.baxter@dark-coin.io.</p>
         <p>
           (This is a sample website - you will be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
       <h2 className="alert">
         <Link href="/alert">
-            <a>Click to test alert.js</a>
+            <a>test alert.js</a>
         </Link>
       </h2>
     </Layout>
