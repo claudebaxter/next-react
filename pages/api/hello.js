d153dd26ffb5167300bd615c61397a873ffb873b
e.baxter@dark-coin.io <e.baxter@dark-coin.io>
2022-09-04 15:52:27 -0500

    Adding example use cast for API routes.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,17 @@
 export default function handler(req, res) {
     res.status(200).json({ text: 'Hello' });
 }
+
+/*A good use case for API Routes is handling form input. 
+For example, you can create a form on your page and have it 
+send a POST request to your API Route. You can then write 
+code to directly save it to your database. The API Route 
+code will not be part of your client bundle, so you can 
+safely write server-side code.
+
+Example:
+export default function handler(req, res) {
+  const email = req.body.email;
+  // Then save email to your database, etc...
+}
+*/
